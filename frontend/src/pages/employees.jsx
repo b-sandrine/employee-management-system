74ce15e8330cc2bbf48b77c1be8e6c1e156a32d0
b-sandrine <sanrdushimimana@gmail.com>
2023-08-17 07:09:14 +0200

    added buttons

--- frontend/src/pages/employees.jsx
+++ frontend/src/pages/employees.jsx
@@ -8,15 +8,21 @@ const Employees = () => {
             <div className="employees">
                 <h3>Company Employees</h3>
                 <div className="line"></div>
-                <div className="options">
-                    <div className="input">
-                        <input type="search" name="" id="" placeholder='Search Employees' />
-                        <MdSearch className='icon' />
+                <div className="operations">
+                    <div className="options">
+                        <div className="input">
+                            <input type="search" name="" id="" placeholder='Search Employees' />
+                            <MdSearch className='icon' />
+                        </div>
+                        <select name="" id="">
+                            <option value="1">Active</option>
+                            <option value="2">Away</option>
+                        </select>
                     </div>
-                    <select name="" id="">
-                        <option value="1">Active</option>
-                        <option value="2">Away</option>
-                    </select>
+                    <div className="buttons">
+                        <button>Add Employee</button>
+                        <button>Import Employee</button>
+                    </div>
                 </div>
             </div>
         </>
